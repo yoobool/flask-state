4ac4acb215ba482400d81c46db6f03e33027c6cd
aaajiulong <525763728@qq.com>
2020-10-12 17:43:02 +0800

    refactor: Delete invalid status code

--- src/flask_state/exceptions/error_code.py
+++ src/flask_state/exceptions/error_code.py
@@ -5,7 +5,6 @@ from . import ErrorCode
 class MsgCode(ErrorCode):
     OVERSTEP_DAYS_SCOPE = {'msg': 'Exceeding the allowed query time range', 'code': '10001'}
     REQUEST_METHOD_ERROR = {'msg': 'The request method cannot be get', 'code': '10002'}
-    ERROR_TYPE = {'msg': 'Wrong parameter type', 'code': '10004'}
-    JSON_FORMAT_ERROR = {'msg': 'JSON format is required', 'code': '10005'}
-    AUTH_FAIL = {'msg': 'Validation failed', 'code': '10006'}
-    UNKNOWN_ERROR = {'msg': 'Unknown error', 'code': '10007'}
+    JSON_FORMAT_ERROR = {'msg': 'JSON format is required', 'code': '10003'}
+    AUTH_FAIL = {'msg': 'Validation failed', 'code': '10004'}
+    UNKNOWN_ERROR = {'msg': 'Unknown error', 'code': '10005'}
