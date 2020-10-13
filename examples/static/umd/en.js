e225146cb5cb8c966186bba8563b9080ee0b2373
aaajiulong <525763728@qq.com>
2020-10-13 16:59:29 +0800

    refactor: Modify i18n files

--- examples/static/umd/en.js
+++ examples/static/umd/en.js
@@ -1,11 +1,23 @@
-const en = {
-    "language": "1", "host_status": "host_status", "cpu": "cpu", "memory": "memory",
+/* Language: English */
+export default {
+    "language": "en",
+    "host_status": "host_status",
+    "cpu": "cpu",
+    "memory": "memory",
     "disk_usage": "disk_usage",
     "load_avg": "LoadAvg",
-    "boot_seconds": "boot_seconds", "redis_status": "redis_status", "used_memory": "used_memory",
-    "used_memory_rss": "used_memory_rss", "mem_fragmentation_ratio": "mem_fragmentation_ratio",
+    "boot_seconds": "boot_seconds",
+    "redis_status": "redis_status",
+    "used_memory": "used_memory",
+    "used_memory_rss": "used_memory_rss",
+    "mem_fragmentation_ratio": "mem_fragmentation_ratio",
     "hits_ratio": "hits_ratio",
-    "delta_hits_ratio": "delta_hits_ratio", "uptime_in_seconds": "uptime_in_seconds",
-    "connected_clients": "connected_clients", "days": "days", "hours": "hours", "minutes": "minutes",
-    "seconds": "seconds", "today": "today"
+    "delta_hits_ratio": "delta_hits_ratio",
+    "uptime_in_seconds": "uptime_in_seconds",
+    "connected_clients": "connected_clients",
+    "days": "days",
+    "hours": "hours",
+    "minutes": "minutes",
+    "seconds": "seconds",
+    "today": "today"
 };
