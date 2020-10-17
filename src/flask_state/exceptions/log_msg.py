200f82cbfd3bcb8328dd3a49d07acf2561452bc0
aaajiulong <525763728@qq.com>
2020-10-17 15:00:41 +0800

    refactor: Add log status information

--- src/flask_state/exceptions/log_msg.py
+++ src/flask_state/exceptions/log_msg.py
@@ -10,3 +10,9 @@ class WarningMsg(ExceptionMsg):
     DATA_TYPE_ERROR = {'msg': 'Data type format error', 'level': 'warning'}
     NO_ACCESS = {'msg': 'Path has no access, now use the default address instead(sqlite:///flask_state_host.db)',
                  'level': 'warning'}
+
+
+class InfoMsg(ExceptionMsg):
+    INSERT_SUCCESS = {'msg': 'Insert status', 'level': 'info'}
+    QUERY_SUCCESS = {'msg': 'Query status', 'level': 'info'}
+    GET_YESTERDAY = {'msg': 'Query yesterday status', 'level': 'info'}
