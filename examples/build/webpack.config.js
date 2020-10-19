5d9c74be32d0ebf30879db57116d75dc00f51bd7
aaajiulong <525763728@qq.com>
2020-10-19 20:56:12 +0800

    chore: Update webpack.config

--- examples/build/webpack.config.js
+++ examples/build/webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = {
+    // umd
+    // entry: './examples/static/umd/flask-state.js',
+    // output: {
+    //     filename: 'flask-state.min.js',
+    //     library: 'flaskState',
+    //     libraryTarget: 'umd',
+    //     path: path.resolve('./examples/static/', 'dist')
+    // },
+
+    // cjs
+    // entry: './examples/static/cjs/flask-state.js',
+    // output: {
+    //     filename: 'flask-state.min.js',
+    //     libraryTarget: "commonjs2",
+    //     path: path.resolve('./examples/static/', 'dist')
+    // },
+
+    // entry
     entry: './examples/static/entry/index.js',
     output: {
-        filename: 'flask-state.js',
+        filename: 'flask-state.min.js',
         path: path.resolve('./examples/static/', 'dist')
     },
     mode: 'development',
@@ -14,14 +36,42 @@ module.exports = {
             loader: 'babel-loader'
         }, {
             test: /\.css$/,
-            use: ['style-loader', 'css-loader']
+            use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        publicPath: '../'
+                    }
+                }, 'css-loader']
         }
         ],
     },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: {
+                    comments: false,
+                },
+                compress: {
+                    pure_funcs: ["console.log"]
+                },
+            },
+            extractComments: false
+        })],
+        splitChunks: {
+            chunks: 'all',
+            name: false,
+        },
+    },
     plugins: [
         new HtmlWebpackPlugin({
-            filename:'../../templates/index.html',
-            template:'./examples/static/entry/index.html',
-        })
+            filename: '../../templates/index.html',
+            template: './examples/static/entry/index.html',
+        }),
+        new MiniCssExtractPlugin({
+            filename: "css/flask-state.css",
+            chunkFilename: 'css/[name].[contenthash:8].chunk.css',
+        }),
+        new optimizeCssAssetsPlugin(),
     ]
 };
