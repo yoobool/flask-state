9a96d5939666361a70b9476533119004a048cdb5
aaajiulong <525763728@qq.com>
2020-10-20 07:14:48 -0400

    refactor: Extract init_app function

--- src/flask_state/controller/manager.py
+++ src/flask_state/controller/manager.py
@@ -1,12 +1,11 @@
 import sys
 import time
-import math
 from concurrent.futures import ThreadPoolExecutor
 
 from flask import request, current_app
 
 from .response_methods import make_response_content
-from ..conf.config import HttpMethod
+from ..conf.config import HttpMethod, Constant
 from ..exceptions import ErrorResponse
 from ..exceptions.error_code import MsgCode
 from ..exceptions.log_msg import ErrorMsg
@@ -18,8 +17,10 @@ from ..utils.file_lock import Lock
 from ..utils.format_conf import format_address, get_file_inf, format_sec
 from ..utils.logger import logger, DefaultLogger
 
+ONE_MINUTE_SECONDS = 60
 
-def init_app(app, interval=60, log_instance=None):
+
+def init_app(app, interval=Constant.DEFAULT_SECONDS, log_instance=None):
     """
     Plugin entry
     :param app: Flask app
@@ -29,39 +30,48 @@ def init_app(app, interval=60, log_instance=None):
     logger.set(log_instance or DefaultLogger().get())
     app.add_url_rule('/v0/state/hoststatus', endpoint='state_host_status', view_func=query_flask_state,
                      methods=[HttpMethod.POST.value])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-    if not app.config.get('SQLALCHEMY_BINDS', {}).get('flask_state_sqlite'):
-        raise KeyError(ErrorMsg.LACK_SQLITE.get_msg())
-    app.config['SQLALCHEMY_BINDS']['flask_state_sqlite'] = format_address(
-        app.config['SQLALCHEMY_BINDS'].get('flask_state_sqlite'))
+    init_db(app)
+    init_redis(app)
+    model_init_app(app)
+
+    # Timing recorder
+    interval = format_sec(interval)
+
+    ThreadPoolExecutor(max_workers=1).submit(record_timer, app, interval)
+
+
+def init_redis(app):
     if app.config.get('REDIS_CONF', {}).get('REDIS_STATUS'):
         redis_state = app.config['REDIS_CONF']
         redis_conf = {'REDIS_HOST': redis_state.get('REDIS_HOST'), 'REDIS_PORT': redis_state.get('REDIS_PORT'),
                       'REDIS_PASSWORD': redis_state.get('REDIS_PASSWORD')}
         redis_conn.set_redis(redis_conf)
-    model_init_app(app)
+
+
+def init_db(app):
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    if not app.config.get('SQLALCHEMY_BINDS', {}).get('flask_state_sqlite'):
+        raise KeyError(ErrorMsg.LACK_SQLITE.get_msg())
+    app.config['SQLALCHEMY_BINDS']['flask_state_sqlite'] = format_address(
+        app.config['SQLALCHEMY_BINDS'].get('flask_state_sqlite'))
+
+
+def record_timer(app, interval):
     app.lock_flask_state = Lock.get_file_lock()
-
-    # Timing recorder
-    interval = format_sec(interval)
-
-    def record_timer():
-        with app.app_context():
-            try:
-                current_app.lock_flask_state.acquire()
-                in_time = time.time()
-                target_time = int(int((time.time()) / 60 + 1) * 60)
-                time.sleep(60 - in_time % 60)
-                while True:
-                    record_flask_state_host()
-                    target_time += interval
-                    now_time = time.time()
-                    time.sleep(target_time - now_time)
-            except Exception as e:
-                current_app.lock_flask_state.release()
-                raise e
-
-    ThreadPoolExecutor(max_workers=1).submit(record_timer)
+    with app.app_context():
+        try:
+            current_app.lock_flask_state.acquire()
+            in_time = time.time()
+            target_time = int(int((time.time()) / ONE_MINUTE_SECONDS + 1) * ONE_MINUTE_SECONDS)
+            time.sleep(ONE_MINUTE_SECONDS - in_time % ONE_MINUTE_SECONDS)
+            while True:
+                record_flask_state_host(interval)
+                target_time += interval
+                now_time = time.time()
+                time.sleep(target_time - now_time)
+        except Exception as e:
+            current_app.lock_flask_state.release()
+            raise e
 
 
 @auth_user
