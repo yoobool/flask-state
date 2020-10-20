835e7fc8e935e6d552435b142e9c0f4e60f59a42
aaajiulong <525763728@qq.com>
2020-10-20 09:17:55 +0800

    docs: Revised version information and description

--- src/flask_state/__about__.py
+++ src/flask_state/__about__.py
@@ -1,6 +1,6 @@
 __title__ = 'Flask-State'
-__version__ = "0.0.3"
-__description__ = 'Monitor host status for Flask'
+__version__ = "1.0.0"
+__description__ = 'A visual plug-in for displaying native state through echarts for browser.'
 __url__ = 'https://github.com/yoobool/flask-state'
 __author__ = 'Yoobool'
 __author_email__ = 'yhl@uuchat.io'
