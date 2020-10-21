001d2baa513806062c41e945a856408bb41d106b
aaajiulong <525763728@qq.com>
2020-10-22 00:45:40 +0800

    chore: Update compress config

--- examples/build/compress.js
+++ examples/build/compress.js
@@ -2,34 +2,72 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const optimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const TerserPlugin = require('terser-webpack-plugin');
-module.exports = {
-    // umd
-    // entry: './examples/static/umd/flask-state.js',
-    // output: {
-    //     filename: 'flask-state.min.js',
-    //     library: 'flaskState',
-    //     libraryTarget: 'umd',
-    //     path: path.resolve('./examples/compress/umd/')
-    // },
 
+module.exports = [{
     // cjs
-    // entry: './examples/static/cjs/flask-state.js',
-    // output: {
-    //     filename: 'flask-state.min.js',
-    //     libraryTarget: "commonjs2",
-    //     path: path.resolve('./examples/compress/cjs/')
-    // },
-
-    // CSS file
-    // entry: './examples/static/css-index.js',
-    // output: {
-    //     path: path.resolve('./examples/compress/cjs/')
-    // },
-    // output: {
-    //     path: path.resolve('./examples/compress/umd/')
-    // },
-
-    mode: 'production',
+    entry: './examples/static/cjs/flask-state.js',
+    output: {
+        filename: 'flask-state.min.js',
+        libraryTarget: "commonjs2",
+        path: path.resolve('./examples/compress/cjs/'),
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }
+        ],
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: {
+                    comments: false,
+                },
+                compress: {
+                    pure_funcs: ["console.log"]
+                },
+            },
+            extractComments: false
+        })],
+    },
+}, {
+    // umd
+    entry: './examples/static/cjs/flask-state.js',
+    output: {
+        filename: 'flask-state.min.js',
+        library: 'flaskState',
+        libraryTarget: 'umd',
+        path: path.resolve('./examples/compress/umd/')
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        }
+        ],
+    },
+    optimization: {
+        minimizer: [new TerserPlugin({
+            terserOptions: {
+                output: {
+                    comments: false,
+                },
+                compress: {
+                    pure_funcs: ["console.log"]
+                },
+            },
+            extractComments: false
+        })],
+    },
+}, {
+    // css
+    entry: './examples/static/css-index.js',
+    output: {
+        path: path.resolve('./examples/compress/')
+    },
     module: {
         rules: [{
             test: /\.js$/,
@@ -47,23 +85,10 @@ module.exports = {
         }
         ],
     },
-    optimization: {
-        minimizer: [new TerserPlugin({
-            terserOptions: {
-                output: {
-                    comments: false,
-                },
-                compress: {
-                    pure_funcs: ["console.log"]
-                },
-            },
-            extractComments: false
-        })],
-    },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "css/flask-state.min.css",
         }),
         new optimizeCssAssetsPlugin(),
     ]
-};
+}];
