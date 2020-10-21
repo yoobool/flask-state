0c55aca3592d075d31750572eb19c8544b9e62f7
aaajiulong <525763728@qq.com>
2020-10-21 16:21:05 +0800

    refactor: Modify test files

--- examples/static/entry/index.js
+++ examples/static/entry/index.js
@@ -1,6 +1,6 @@
+import {init} from '../cjs';
 import '../cjs/flask-state.css';
-import {zh} from 'flask-state/i18n.js';
-const flaskState = require('flask-state');
-flaskState.init(null);
-flaskState.init(document.getElementById('test_id'), zh);
+import {zh} from '../cjs/i18n.js';
+init(null);
+init(document.getElementById('test_id'), zh);
 
