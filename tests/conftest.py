fc97c522f9821a9dc4216fbc0d85b863b3242444
aaajiulong <525763728@qq.com>
2020-10-26 20:19:54 +0800

    test: Update conftest and route test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,64 +1,15 @@
 import flask
 import pytest
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import func
-from sqlalchemy.sql import text
+
+from src.flask_state import DEFAULT_BIND_SQLITE
 
 
 @pytest.fixture
-def app(request):
+def app():
     app = flask.Flask('test_app')
     app.testing = True
-    app.config['SQLALCHEMY_BINDS'] = {'flask_state_sqlite': 'sqlite:///test.db'}
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    app.config['SQLALCHEMY_BINDS'] = {DEFAULT_BIND_SQLITE: 'sqlite:///test.db'}
     app.config['REDIS_CONF'] = {'REDIS_STATE': True, 'REDIS_HOST': '192.168.0.2', 'REDIS_PORT': 16379,
                                 'REDIS_PASSWORD': 'fish09'}
     return app
-
-
-@pytest.fixture
-def db(app):
-    return SQLAlchemy(app)
-
-
-@pytest.fixture
-def host(db):
-    class FlaskStateHost(db.Model):
-        __bind_key__ = 'flask_state_sqlite'
-        __tablename__ = "flask_state_host"
-
-        __table_args__ = (
-            db.PrimaryKeyConstraint('id'),
-            {
-                'extend_existing': True,
-            }
-        )
-        id = db.Column(db.Integer, autoincrement=True)
-        create_time = db.Column(db.DateTime, server_default=func.now())
-        update_time = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
-
-        # host
-        cpu = db.Column(db.Float, server_default=text("0"))
-        memory = db.Column(db.Float, server_default=text("0"))
-        load_avg = db.Column(db.String(32), server_default="")
-        disk_usage = db.Column(db.Float, server_default=text("0"))
-        boot_seconds = db.Column(db.Integer, server_default=text("0"))
-        ts = db.Column(db.Integer, server_default=text("0"))
-
-        # redis
-        used_memory = db.Column(db.Integer, server_default=text("0"))
-        used_memory_rss = db.Column(db.Integer, server_default=text("0"))
-        connected_clients = db.Column(db.SmallInteger, server_default=text("0"))
-        uptime_in_seconds = db.Column(db.Integer, server_default=text("0"))
-        mem_fragmentation_ratio = db.Column(db.Float, server_default=text("0"))
-        keyspace_hits = db.Column(db.Integer, server_default=text("0"))
-        keyspace_misses = db.Column(db.Integer, server_default=text("0"))
-        hits_ratio = db.Column(db.Float, server_default=text("0"))
-        delta_hits_ratio = db.Column(db.Float, server_default=text("0"))
-
-        def __repr__(self):
-            return "<FlaskStateHost cpu: %s, memory:%s, load_avg:%s, disk_usage:%s, boot_seconds:%s, ts:%s>" % (
-                self.cpu, self.memory, self.load_avg, self.disk_usage, self.boot_seconds, self.ts)
-
-    db.create_all()
-    yield FlaskStateHost
-    db.drop_all()
--- tests/test_route.py
+++ tests/test_route.py
@@ -1,27 +1,14 @@
 from flask import json
 
-from src.flask_state import query_console_status
+from src.flask_state.conf import config
+from src.flask_state.controller import manager
+from src.flask_state.models import model_init_app
 
 
-def test_query_host(app, host):
-    app.add_url_rule('/v0/state/hoststatus', endpoint='state_host_status', view_func=query_console_status,
-                     methods=['GET', 'POST'])
-
+def test_init_app(app):
+    model_init_app(app)
+    app.add_url_rule('/v0/state/hoststatus', endpoint='state_host_status', view_func=manager.query_flask_state,
+                     methods=[config.HttpMethod.POST.value])
     c = app.test_client()
-
-    def host_and_assert(code='200', msg='Search success', data=None):
-        data = json.dumps(data)
-        rv = c.post('/v0/state/hoststatus', data=data)
-        result = json.loads(rv.data)
-        assert result.get('code') == code
-        assert result.get('msg') == msg
-
-    host_and_assert('10005', 'JSON format is required')
-
-    host_and_assert('10001', 'Exceeding the allowed query time range', {'1': '1'})
-
-    host_and_assert(data={'timeQuantum': '1'})
-
-    rv = c.get('/v0/state/hoststatus')
-    result = json.loads(rv.data)
-    assert result.get('code') == '10002'
+    response = c.post('/v0/state/hoststatus', data=json.dumps({'timeQuantum': 1}), content_type='application/json')
+    assert 200 == response.status_code
