cc2ec3a3aab48b5e110f51ecdd1e2407b6f7b3aa
aaajiulong <525763728@qq.com>
2020-10-26 13:40:35 +0800

    test: Update utils files unit test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,82 @@
+import json
 import os
+import sys
+import time
+
+from flask import make_response
 
 from src.flask_state.conf.config import Constant
 from src.flask_state.exceptions.log_msg import ErrorMsg
-from src.flask_state.utils import format_conf
+from src.flask_state.utils import format_conf, date, file_lock, auth
+
+
+# auth
+def test_auth_method(app):
+    """
+    Test whether the different request types can be verified correctly
+    :param app: test Flask(__name__)
+    """
+
+    @app.route('/test_method', methods=['GET', 'POST'])
+    @auth.auth_method
+    def test_method():
+        return make_response()
+
+    c = app.test_client()
+    post_response = c.post('/test_method')
+    get_response = c.get('/test_method')
+
+    ok = 200
+    error = 401
+    error_request_method = 'The request method cannot be get'
+    assert ok == post_response.status_code
+    assert error == get_response.status_code
+    assert error_request_method == json.loads(str(get_response.data, 'utf-8')).get('msg')
+
+
+# date
+def test_get_current_ms():
+    """
+    Test whether the current MS is obtained correctly
+    """
+    from src.flask_state.utils.date import SECONDS_TO_MILLISECOND_MULTIPLE
+    now_ms = int(round(time.time() * SECONDS_TO_MILLISECOND_MULTIPLE))
+    assert now_ms == date.get_current_ms()
+
+
+def test_get_current_s():
+    """
+    Test whether the current second is obtained correctly
+    """
+    now_s = int(round(time.time()))
+    assert now_s == date.get_current_s()
+
+
+def test_get_query_ms():
+    """
+    Test whether the obtained query milliseconds are correct
+    """
+    test_query_list = {1: 86400000, 3: 259200000, 7: 604800000, 30: 2592000000, -1: 0, 5: 0, 14: 0,
+                       100: 0}  # Only 1, 3, 7, 30 can be queried correctly
+    for query in test_query_list:
+        assert test_query_list.get(query) == date.get_query_ms(query)
+
+
+# file_lock
+def test_file_lock():
+    """
+    Test whether the file lock is successful
+    """
+    lock = file_lock.Lock.get_file_lock()
+    lock_copy = file_lock.Lock.get_file_lock()
+    assert not lock.acquire()
+    try:
+        lock_copy.acquire()
+    except BlockingIOError as e:
+        assert e.__str__() == "[Errno 35] Resource temporarily unavailable"
+
+    lock.release()
+    assert not lock_copy.acquire()
 
 
 # format_conf
@@ -69,7 +143,7 @@ def test_format_address():
             assert v.__str__() == 'Incorrect address format, Set the format to: sqlite:///path. Error Sqlite url: %s' % address
     assert target_address_error_count == len(test_address_list) - 1
 
-    correct_address_list = ['sqlite:///test.db']
+    correct_address_list = ['sqlite:///test.db', 'sqlite:////']
     for address in correct_address_list:
         index = address[Constant.MIN_ADDRESS_LENGTH - 1:].rfind('/')
         try:
@@ -77,4 +151,13 @@ def test_format_address():
                 raise ValueError(
                     ErrorMsg.NO_ACCESS.get_msg('. No access path: %s' % address[Constant.MIN_ADDRESS_LENGTH - 1:index]))
         except ValueError as v:
-            assert v.__str__() == '. No access path: %s' % address[Constant.MIN_ADDRESS_LENGTH - 1:index]
+            assert v.__str__() == 'Path has no access, make sure you have access to the path. No access path: %s' \
+                   % address[Constant.MIN_ADDRESS_LENGTH - 1:index]
+
+
+def test_get_file_inf():
+    """
+    Test whether the function name is normal
+    """
+    result = format_conf.get_file_inf(sys._getframe())
+    assert 'test_get_file_inf' == result.get('handlerFuncName')
