85681c8f43256799abcd4edc7ab4381f6c0b369e
caixiaoyue <caixiaoyue@uuchat.io>
2020-10-30 09:32:42 +0800

    feature: Add Japanese translation

--- packages/i18n.js
+++ packages/i18n.js
@@ -46,4 +46,28 @@ const zh = {
     "today": "今天"
 };
 
-export {zh, en};
+/* Language: Japanese */
+const ja = {
+    "language": "ja",
+    "host_status": "ホストステータス",
+    "cpu": "CPU",
+    "memory": "メモリー",
+    "disk_usage": "ディスクの使用率",
+    "load_avg": "平均負荷",
+    "boot_seconds": "起動秒",
+    "redis_status": "Redisステータス",
+    "used_memory": "総メモリ",
+    "used_memory_rss": "使用済みメモリ",
+    "mem_fragmentation_ratio": "Memレシオ",
+    "hits_ratio": "ヒット率",
+    "delta_hits_ratio": "Deltaヒット率",
+    "uptime_in_seconds": "の稼働時間",
+    "connected_clients": "現在の接続",
+    "days": "日々",
+    "hours": "アワー",
+    "minutes": "ぶん",
+    "seconds": "びょう",
+    "today": "ょう"
+};
+
+export {zh, en, ja};
---
+++ packages/umd/ja.js
@@ -0,0 +1,25 @@
+/* Language: Japanese */
+const ja = {
+    "language": "ja",
+    "host_status": "ホストステータス",
+    "cpu": "CPU",
+    "memory": "メモリー",
+    "disk_usage": "ディスクの使用率",
+    "load_avg": "平均負荷",
+    "boot_seconds": "起動秒",
+    "redis_status": "Redisステータス",
+    "used_memory": "総メモリ",
+    "used_memory_rss": "使用済みメモリ",
+    "mem_fragmentation_ratio": "Memレシオ",
+    "hits_ratio": "ヒット率",
+    "delta_hits_ratio": "Deltaヒット率",
+    "uptime_in_seconds": "の稼働時間",
+    "connected_clients": "現在の接続",
+    "days": "日々",
+    "hours": "アワー",
+    "minutes": "ぶん",
+    "seconds": "びょう",
+    "today": "ょう"
+};
+
+flaskState.ja = ja;
