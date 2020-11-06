125b388070953ebb90340650512ca454c1dda517
aaajiulong <525763728@qq.com>
2020-11-06 10:14:11 +0800

    refactor: Modify examples files

--- examples/static/entry/index.js
+++ examples/static/entry/index.js
@@ -1,5 +1,5 @@
 import {init} from '../../../packages';
-import '../../../packages/flask-state.min.css';
+import '../../../packages/flask-state.css';
 import {zh} from '../../../packages/i18n';
 init(null);
 init({dom:document.getElementById('test_id'), lang:zh});
