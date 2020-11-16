7b219beacd793c051640793326503c22ea116f94
caixiaoyue <caixiaoyue@uuchat.io>
2020-11-16 10:57:37 +0800

    feature: Added Spanish and Indonesian

---
+++ packages/umd/es.js
@@ -0,0 +1,25 @@
+/* Language: Spanish */
+const es = {
+    "language": "es",
+    "host_status": "Estado del host",
+    "cpu": "CPU",
+    "memory": "Memoria",
+    "disk_usage": "Uso del disco",
+    "load_avg": "Promedio de carga",
+    "boot_seconds": "Tiempo de actividad",
+    "redis_status": "Estado de Redis",
+    "used_memory": "Memoria total",
+    "used_memory_rss": "Usar memoria",
+    "mem_fragmentation_ratio": "Fragmentación de mem",
+    "hits_ratio": "Aciertos de caché",
+    "delta_hits_ratio": "Aciertos de caché de 24 horas",
+    "uptime_in_seconds": "Tiempo de actividad",
+    "connected_clients": "Conexiones",
+    "days": "dias",
+    "hours": "horas",
+    "minutes": "min",
+    "seconds": "segundos",
+    "today": "Hoy"
+};
+
+flaskState.es = es;
