94311948cbd6fa8d800ae903db406ac1658be1f0
aaajiulong <525763728@qq.com>
2020-12-01 14:50:29 +0800

    styles: Delete redundant behavior

--- examples/config.py
+++ examples/config.py
@@ -17,7 +17,6 @@ def setting_app():
     import os
 
     path_ = os.getcwd() + "/flask_state_host.db"
-    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["SQLALCHEMY_BINDS"] = {DEFAULT_BIND_SQLITE: "sqlite:///" + path_}
 
     # log_instance = logging.getLogger(__name__)
