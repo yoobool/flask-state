33e2fd0e26e83d86755920ec61c04db8e3970fb2
aaajiulong <525763728@qq.com>
2020-12-08 09:15:46 +0800

    chore: Delete redundant fields

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ with open("src/flask_state/__about__.py") as f:
     exec(f.read(), about)
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
-    name=about["__title__"],
     version=about["__version__"],
     install_requires=["Flask>=1.0", "Flask-SQLAlchemy>=1.0", "psutil>=5.7.0"],
 )
--- src/flask_state/__about__.py
+++ src/flask_state/__about__.py
@@ -1,8 +1,1 @@
-__title__ = "Flask-State"
 __version__ = "1.0.5"
-__description__ = "A visual plug-in for displaying native state through echarts for browser."
-__url__ = "https://github.com/yoobool/flask-state"
-__author__ = "Yoobool"
-__author_email__ = "yhl@uuchat.io"
-__maintainer__ = "Yoobool"
-__license__ = "BSD-3"
