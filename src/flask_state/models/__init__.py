c34d0e5c78639b8a6da14920a3677bcf932d08b0
aaajiulong <525763728@qq.com>
2021-03-23 09:13:28 +0800

    feature: The database is automatically migrated when the model is initialized

--- src/flask_state/models/__init__.py
+++ src/flask_state/models/__init__.py
@@ -1,11 +1,43 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import String
 
 from ..conf.config import Config
+from ..migrate import upgrade
 
 db = SQLAlchemy()
 
 
+class AlembicVersion(db.Model):
+    __bind_key__ = Config.DEFAULT_BIND_SQLITE
+    __tablename__ = "alembic_version"
+
+    version_num = db.Column(String(32), unique=True)
+
+    __table_args__ = (
+        db.PrimaryKeyConstraint("version_num"),
+        db.Index("alembic_version_pkc", version_num),
+    )
+
+
+class _Migrate(object):
+    def __init__(self, db, **kwargs):
+        self.db = db
+        self.configure_args = kwargs
+
+
 def model_init_app(app):
-    db.init_app(app)
     with app.app_context():
-        db.create_all(bind=Config.DEFAULT_BIND_SQLITE)
+        db.init_app(app)
+        app.extensions["migrate"] = _Migrate(db)
+        tables = [table.name for table in db.get_tables_for_bind(bind=Config.DEFAULT_BIND_SQLITE)]
+        try:
+            if tables:
+                if Config.ALEMBIC_VERSION not in tables:
+                    upgrade(app)
+                else:
+                    is_new = db.session.query(AlembicVersion.version_num == Config.DB_VERSION).first()
+                    if not is_new:
+                        upgrade(app)
+        except:
+            pass
+        db.create_all(bind=Config.DEFAULT_BIND_SQLITE, app=app)
