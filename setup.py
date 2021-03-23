ee89b56573534b1baee274bcb46845965ae9eaf9
aaajiulong <525763728@qq.com>
2021-03-23 15:40:51 +0800

    chore: install_requires add alembic

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("src/flask_state/__about__.py") as f:
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     version=about["__version__"],
-    install_requires=["Flask>=1.0", "Flask-SQLAlchemy>=1.0", "psutil>=5.7.0"],
+    install_requires=["Flask>=1.0", "Flask-SQLAlchemy>=1.0", "psutil>=5.7.0", "alembic>=1.4.3"],
     extras_require={
         "test": ["pytest", "redis"],
         "dev": ["black", "codespell", "flake8", "isort", "pip-tools", "pre-commit", "setuptools", "twine", "wheel"],
